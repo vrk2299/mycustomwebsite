9f90bb914c896cebe08f524e3a6df3826fd794f3
vrk2299 <vivekreddykompelly99@gmail.com>
2024-04-30 08:58:26 +0530

    first commit

---
+++ file.py
@@ -0,0 +1,6 @@
+#hello this is a sample python text
+
+for i in {1,2,3,4,5} :
+    print(int(i)+20)
+
+
---
+++ readme.txt
@@ -0,0 +1,3 @@
+#hello this is a sample readme .txt
+
+this is the line where the git is tracking .
